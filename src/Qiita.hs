50a35fca86b1b39f598163db16931c774051b5f3
mathhun <achemer@gmail.com>
2015-10-21 14:53:08 +0900

    show article title

--- src/Qiita.hs
+++ src/Qiita.hs
@@ -7,19 +7,21 @@ import Text.HTML.Scalpel
 import Data.ByteString (ByteString)
 import qualified Data.ByteString.Char8 as C
 
-data Site = Site { siteTitle :: ByteString
-                 , siteSnippets :: [Snippet]
-                 }
+data Article = Article { aTitle :: ByteString
+                       , aSnippets :: [Snippet]
+                       }
 
 data Snippet = Snippet { snptFile :: ByteString
                        , snptCode :: ByteString
                        } deriving Show
 
-allSnippets :: URL -> IO (Maybe [Snippet])
+allSnippets :: URL -> IO (Maybe Article)
 allSnippets url = scrapeURL url snippets
 
-snippets :: Scraper ByteString [Snippet]
-snippets = chroots ("div" @: [hasClass "code-frame"]) snippet
+snippets :: Scraper ByteString Article
+snippets = Article
+           <$> text "title"
+           <*> chroots ("div" @: [hasClass "code-frame"]) snippet
 
 snippet :: Scraper ByteString Snippet
 snippet = Snippet <$> file <*> code
@@ -28,26 +30,30 @@ snippet = Snippet <$> file <*> code
           code :: Scraper ByteString ByteString
           code = text "pre"
 
-printSnippetList :: [Snippet] -> IO ()
-printSnippetList snpts = mapM_ print1 $ zip [1..] snpts
+printSnippetList :: Article -> IO ()
+printSnippetList article = do
+  C.putStrLn $ (C.pack "Title: ") `C.append` aTitle article
+  mapM_ print1 $ zip [1..] $ aSnippets article
     where print1 (n, snpt) = C.putStrLn $ (C.pack $ show n ++ " ") `C.append` (snptFile snpt)
 
-runDownloadSnippets :: Snippet -> IO ()
-runDownloadSnippets snpt = do C.putStrLn $ snptFile snpt
-                              C.putStrLn $ snptCode snpt
+runDownloadSnippets :: Article -> IO ()
+runDownloadSnippets article = do
+  let snpt = head $ aSnippets article
+  C.putStrLn $ snptFile snpt
+  C.putStrLn $ snptCode snpt
 
 run :: IO ()
 run = do
   (url:_) <- getArgs
-  snpts <- allSnippets url
-  maybe printError processSnippets snpts
+  article <- allSnippets url
+  maybe printError processSnippets article
     where
       printError = putStrLn "ERROR: Could not scrape the URL"
-      processSnippets snpts = do
-          case length snpts of
+      processSnippets article = do
+          case length (aSnippets article) of
             0 -> putStrLn "No snippet found"
-            1 -> runDownloadSnippets $ head snpts
-            _ -> printSnippetList snpts
+            1 -> runDownloadSnippets article
+            _ -> printSnippetList article
 
 -- option -o (filename)
 -- option -n to specify snpt
